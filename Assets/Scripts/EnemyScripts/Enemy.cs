0e7ba13e6e52f7070fbacca6c11bf9ade17da1ea
JuulScripts <jscripts11@gmail.com>
2025-06-16 18:00:11 +0200

    added basic tower

--- Assets/Scripts/EnemyScripts/Enemy.cs
+++ Assets/Scripts/EnemyScripts/Enemy.cs
@@ -1,19 +1,38 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private float health;
      public int speed;
+    private Animator animator;
 
 
+
+    public enum EnemyStates
+    {
+        Moving,
+        Attacking,
+        Idle
+    }
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        move();
+    }
+    public EnemyStates state = EnemyStates.Idle;
     public void DecreaseHealth(float amount)
     {
         health -= amount;
     }
+    
+
 
 
     public void move()
     {
-
+        state = EnemyStates.Moving;
+        print((int)state);
+        animator.SetInteger("State", (int)state);
     }
 }
--- Assets/Scripts/EnemyScripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyScripts/EnemyBehaviour.cs
@@ -1,12 +1,17 @@
+using System;
+using UnityEditor.PackageManager;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
 
 public class EnemyBehaviour : MonoBehaviour
 {
-    [SerializeField] private GameObject[] waypoints;  
+    [SerializeField] private GameObject[] waypoints;
+    [SerializeField] private int rotationSpeed;
     private Enemy Enemy;
     private float MoveSpeed;
-    private bool shouldmove;
+    private bool shouldmove = true;
+    private int currentindex;
+
     private void Start()
     {
         Enemy = GetComponent<Enemy>();
@@ -15,25 +20,38 @@ public class EnemyBehaviour : MonoBehaviour
 
     }
 
-    private void MoveToWayPoint(GameObject waypoint)
+    private void MoveToWayPoints()
     {
-        if (shouldmove)
+        GameObject waypoint;
+      
+        if (currentindex >= 0 && currentindex < waypoints.Length)
         {
+            waypoint = waypoints[currentindex];
+        } else
+        {
+            Debug.LogError($"{currentindex} is out of bounds. Please provide an currentindex within the range of the 'waypoints' array.");
+            return;
+        }
+
+        if (Enemy.state == Enemy.EnemyStates.Moving && (transform.position - waypoint.transform.position).magnitude >= 0.1)
+        {
+         
             Vector3 Wpos = waypoint.transform.position;
             Vector3 direction = Wpos - transform.position;
-
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
             transform.position += direction.normalized * MoveSpeed;
 
+        } else if (Enemy.state == Enemy.EnemyStates.Moving && currentindex+1 >= 0 && currentindex+1 < waypoints.Length) {
+            currentindex++;
         }
     }
-    public void MoveToWayPoints()
-    {
-        
-    }
+  
 
     private void Update()
     {
-        MoveToWayPoint (waypoints[0]);
+        MoveToWayPoints();
+
     }
 
 }
